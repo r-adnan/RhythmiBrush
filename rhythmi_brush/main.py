7f729427e497af8b5d9191485c1a655e0bcee0b0
Nabil <105045605+LekmineNabil@users.noreply.github.com>
2023-10-07 14:38:13 -0400

    Added video cap

--- rhythmi_brush/main.py
+++ rhythmi_brush/main.py
@@ -8,15 +8,37 @@ import sys
 import os
 
 # Third-party imports
-import cv2
+import cv2 as cv
 import numpy as np
 
 
+def videoProcess():
+    cap = cv.VideoCapture(0)
 
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+
+    while True:
+        # We should capture frame-by-frame footage
+        ret, frame = cap.read()
+        if not ret:
+            print("Can't recieve frame, Exiting...")
+            break
+
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        cv.imshow(frame, gray)
+        
+        if cv.waitKey(1) == ord('q'):
+            break
+        
+    cap.release()
+    cv.destroyAllWindows()
 
 def main():
-    print("Hello")
-    
+    # print("Hello")
+    videoProcess()
+
 
 
 
