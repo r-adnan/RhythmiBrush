ce66c1915924a0227aa00d3b17016e2f0c91ca56
Nabil <105045605+LekmineNabil@users.noreply.github.com>
2023-10-07 15:11:38 -0400

    Added requirements.txt

--- rhythmi_brush/main.py
+++ rhythmi_brush/main.py
@@ -12,7 +12,7 @@ import cv2 as cv
 import numpy as np
 
 
-def videoProcess():
+def videoCap():
     cap = cv.VideoCapture(0)
 
     if not cap.isOpened():
@@ -36,7 +36,7 @@ def videoProcess():
 
 def main():
     # print("Hello")
-    videoProcess()
+    videoCap()
 
 
 
