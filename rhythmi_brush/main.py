817d665db72dc5606e04637781c7a34528f2a770
Nabil <105045605+LekmineNabil@users.noreply.github.com>
2023-10-07 14:29:30 -0400

    setup the main file and whether it runs properly

---
+++ rhythmi_brush/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+'''
+    RhythmiBrush - dynamic airbrush that changes based on music
+'''
+# standard lib imports
+import sys
+import os
+
+# Third-party imports
+import cv2
+import numpy as np
+
+
+
+
+def main():
+    print("Hello")
+    
+
+
+
+if __name__ == "__main__":
+    main()
